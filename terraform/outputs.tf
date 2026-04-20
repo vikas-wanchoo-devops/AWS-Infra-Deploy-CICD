39f5b9f45b95232ea9de0e2227347aaae63518c2
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-20 14:07:56 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,11 @@
 output "ecr_repository_url" {
-  description = "ECR repo URL"
-  value       = aws_ecr_repository.app_repo.repository_url
+  value = aws_ecr_repository.app_repo.repository_url
 }
 
 output "ecs_cluster_name" {
-  description = "ECS cluster name"
-  value       = aws_ecs_cluster.app_cluster.name
+  value = aws_ecs_cluster.app_cluster.name
 }
 
 output "ecs_service_name" {
-  description = "ECS service name"
-  value       = aws_ecs_service.app_service.name
+  value = aws_ecs_service.app_service.name
 }
