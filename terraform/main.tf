c2d07fdd0547dc67be900251cc69b97a855e681d
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-20 14:07:23 +0530

    Remove output variables from main.tf
    Removed output variables for ECR repository, ECS cluster, and ECS service.

--- terraform/main.tf
+++ terraform/main.tf
@@ -85,18 +85,3 @@ resource "aws_ecs_service" "app_service" {
     assign_public_ip = true
   }
 }
-
-# ------------------------------------------------------------
-# 6. Outputs
-# ------------------------------------------------------------
-output "ecr_repository_url" {
-  value = aws_ecr_repository.app_repo.repository_url
-}
-
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.app_cluster.name
-}
-
-output "ecs_service_name" {
-  value = aws_ecs_service.app_service.name
-}
