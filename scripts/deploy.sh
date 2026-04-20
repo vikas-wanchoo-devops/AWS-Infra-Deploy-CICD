00643a482ef74e2bf0699bcf65580b65205a8d9b
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-20 19:50:17 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 aws ecs update-service \
-  --cluster assaabloy-cluster \
-  --service assaabloy-service \
+  --cluster assaabloy-app-cluster \
+  --service assaabloy-app-service \
   --force-new-deployment \
   --region ${{ secrets.AWS_REGION }}
