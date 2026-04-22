a50385cf9b33635955f07220b19759f9e04f39f7
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-22 13:27:24 +0530

    Add VPC ID and subnets variables for Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,19 @@ variable "app_name" {
   type        = string
   default     = "assaabloy-app"
 }
+
+variable "vpc_id" {
+  description = "VPC ID where ECS and ALB will run"
+  type        = string
+  default     = "vpc-0b5d7248bdde16ef7"
+}
+
+variable "subnets" {
+  description = "Subnets for ALB and ECS tasks"
+  type        = list(string)
+  default     = [
+    "subnet-0d16d36a33d1c1f22",
+    "subnet-013e51f5fbc1318cb",
+    "subnet-0a4e24f116d3364f9"
+  ]
+}
