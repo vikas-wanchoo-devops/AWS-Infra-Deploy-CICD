80461df051d30023b51006e88107f2dde7e718c2
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-22 14:43:42 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,3 +25,22 @@ variable "subnets" {
     "subnet-0a4e24f116d3364f9"
   ]
 }
+
+# --- Missing variables added here ---
+variable "execution_role_arn" {
+  description = "ARN of the ECS task execution role"
+  type        = string
+  default     = "arn:aws:iam::123456789012:role/ecsTaskExecutionRole"
+}
+
+variable "task_role_arn" {
+  description = "ARN of the ECS task role"
+  type        = string
+  default     = "arn:aws:iam::123456789012:role/ecsTaskRole"
+}
+
+variable "ecr_repo_url" {
+  description = "ECR repository URL for the app image"
+  type        = string
+  default     = "123456789012.dkr.ecr.eu-north-1.amazonaws.com/assaabloy-app"
+}
